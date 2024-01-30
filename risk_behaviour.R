171885ff91c29f9a928d7b61a7f62e7f3c9facdb
progressable-tech <exercise.journaling@gmail.com>
2024-01-30 21:22:13 +0000

    re-fractored code

--- risk_behaviour.R
+++ risk_behaviour.R
@@ -31,7 +31,106 @@ conn <- dbConnect(RSQLite::SQLite(), dbname = db_path)
 
 ################################################################################
 print_and_save(
-  "######### Is there a difference in risk taking behaviour from baseline? ########"
+  "## Is there a difference in overall average inflates between study conditions? ##"
+)
+################################################################################
+
+print_and_save(
+  "for the game played with the robot, is there a difference in average inflates between the two study conditions?"
+)
+
+overall_avg_query <- "
+  SELECT
+    balloon_inflate.player_id,
+    players.customise_first AS study_cond,
+    AVG(total_inflates - total_inflates_before_help_request) AS overall_avg_inflate_after_help
+  FROM
+    balloon_inflate
+  JOIN
+    players ON balloon_inflate.player_id = players.player_id
+  WHERE
+    players.testing = 0
+    AND players.game_completed = 1
+    AND game_round = 2
+    AND help_requested = 1
+    AND balloon_inflate.balloon_id > 5
+  GROUP BY
+    balloon_inflate.player_id,
+    players.customise_first
+"
+
+overall_avg_data <- dbGetQuery(conn, overall_avg_query)
+
+# Convert 'study_cond' to a factor so the statistical tests know what the categories (study conditions) are
+overall_avg_data$study_cond <-
+  as.factor(overall_avg_data$study_cond)
+
+customise <- subset(overall_avg_data, study_cond == 1)
+mean_custom <- mean(customise$overall_avg_inflate_after_help)
+sd_custum <- sd(customise$overall_avg_inflate_after_help)
+customise_filtered <- subset(customise, overall_avg_inflate_after_help >= (mean_custom - 2 * sd_custum) & overall_avg_inflate_after_help <= (mean_custom + 2 * sd_custum))
+
+non_customise <- subset(overall_avg_data, study_cond == 0)
+mean_non_custum <- mean(non_customise$overall_avg_inflate_after_help)
+sd_non_custum <- sd(non_customise$overall_avg_inflate_after_help)
+non_customise_filtered <- subset(non_customise, overall_avg_inflate_after_help >= (mean_non_custum - 2 * sd_non_custum) & overall_avg_inflate_after_help <= (mean_non_custum + 2 * sd_non_custum))
+
+# Find excluded participants for each study conditions
+excluded_cust_ppt <- setdiff(customise$player_id, customise_filtered$player_id)
+excluded_non_cust_ppt <- setdiff(non_customise$player_id, non_customise_filtered$player_id)
+
+# Combine excluded participant lists for each set of conditions
+all_excluded_ppt <- unique(c(excluded_cust_ppt, excluded_non_cust_ppt))
+
+# Exclude these participants from respective datasets
+customise_filtered <- customise_filtered[!customise_filtered$player_id %in% all_excluded_ppt, ]
+non_customise_filtered <- non_customise_filtered[!non_customise_filtered$player_id %in% all_excluded_ppt, ]
+
+combined_overall_sutdy_cond_data <- rbind(customise_filtered, non_customise_filtered)
+
+perform_shapiro_test(
+  customise_filtered$overall_avg_inflate_after_help,
+  "overall_avg_inflate_after_help for customised condition"
+)
+
+qqPlot(customise_filtered$overall_avg_inflate_after_help)
+
+perform_shapiro_test(
+  non_customise_filtered$overall_avg_inflate_after_help,
+  "overall_avg_inflate_after_help non-customise condition"
+)
+
+qqPlot(non_customise_filtered$overall_avg_inflate_after_help)
+
+# Levene's Test for Homogeneity of Variances
+perform_levene_test(
+  data = combined_overall_sutdy_cond_data,
+  formula = overall_avg_inflate_after_help ~ study_cond
+)
+
+perform_wilcox_rank_sum_test(
+  data = combined_overall_sutdy_cond_data,
+  formula = overall_avg_inflate_after_help ~ study_cond,
+  "avg Inflates between study condition"
+)
+
+ggplot(combined_overall_sutdy_cond_data, aes(x = overall_avg_inflate_after_help, y = factor(study_cond), fill = factor(study_cond))) +
+  geom_density_ridges(alpha = 0.4, scale = 0.6) +
+  geom_boxplot(aes(y = factor(study_cond), x = overall_avg_inflate_after_help), width = 0.2, alpha = 0.3) +
+  geom_jitter(height = 0.1, size = 1, alpha = 0.5, color = "black") +
+  labs(
+    title = "Avg inflates after requesting help from robot per study condition",
+    x = "Avg inflates after help requested",
+    y = "Study Condition"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none", plot.margin = margin(t = 0.5, r = 0.1, b = 0.1, l = 0.5, unit = "cm")) +
+  coord_flip() +
+  scale_y_discrete(expand = c(0, 0), labels = c("non-custom", "custom"))
+
+################################################################################
+print_and_save(
+  "\n\n######### Is there a difference in risk taking behaviour from baseline? ########"
 )
 ################################################################################
 
@@ -160,103 +259,6 @@ ggplot(game_stats, aes(x = game_round, y = mean, fill = game_round)) +
   ) +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5))
-
-################################################################################
-print_and_save(
-  "\n\n## Is there a difference in overall average inflates between study conditions? ##"
-)
-################################################################################
-
-print_and_save(
-  "for the game played with the robot, is there a difference in average inflates between the two study conditions?"
-)
-
-overall_avg_query <- "
-  SELECT
-    balloon_inflate.player_id,
-    players.customise_first AS study_cond,
-    AVG(total_inflates - total_inflates_before_help_request) AS overall_avg_inflate_after_help
-  FROM
-    balloon_inflate
-  JOIN
-    players ON balloon_inflate.player_id = players.player_id
-  WHERE
-    players.testing = 0
-    AND players.game_completed = 1
-    AND game_round = 2
-    AND help_requested = 1
-    AND balloon_inflate.balloon_id > 5
-  GROUP BY
-    balloon_inflate.player_id,
-    players.customise_first
-"
-
-overall_avg_data <- dbGetQuery(conn, overall_avg_query)
-
-# Convert 'study_cond' to a factor so the statistical tests know what the categories (study conditions) are
-overall_avg_data$study_cond <-
-  as.factor(overall_avg_data$study_cond)
-
-customise <- subset(overall_avg_data, study_cond == 1)
-mean_custom <- mean(customise$overall_avg_inflate_after_help)
-sd_custum <- sd(customise$overall_avg_inflate_after_help)
-customise_filtered <- subset(customise, overall_avg_inflate_after_help >= (mean_custom - 2 * sd_custum) & overall_avg_inflate_after_help <= (mean_custom + 2 * sd_custum))
-perform_shapiro_test(
-  customise_filtered$overall_avg_inflate_after_help,
-  "overall_avg_inflate_after_help for customised condition"
-)
-
-qqPlot(customise_filtered$overall_avg_inflate_after_help)
-
-non_customise <- subset(overall_avg_data, study_cond == 0)
-mean_non_custum <- mean(non_customise$overall_avg_inflate_after_help)
-sd_non_custum <- sd(non_customise$overall_avg_inflate_after_help)
-non_customise_filtered <- subset(non_customise, overall_avg_inflate_after_help >= (mean_non_custum - 2 * sd_non_custum) & overall_avg_inflate_after_help <= (mean_non_custum + 2 * sd_non_custum))
-perform_shapiro_test(
-  non_customise_filtered$overall_avg_inflate_after_help,
-  "overall_avg_inflate_after_help non-customise condition"
-)
-
-qqPlot(non_customise_filtered$overall_avg_inflate_after_help)
-
-# Find excluded participants for each study conditions
-excluded_cust_ppt <- setdiff(customise$player_id, customise_filtered$player_id)
-excluded_non_cust_ppt <- setdiff(non_customise$player_id, non_customise_filtered$player_id)
-
-# Combine excluded participant lists for each set of conditions
-all_excluded_ppt <- unique(c(excluded_cust_ppt, excluded_non_cust_ppt))
-
-# Exclude these participants from respective datasets
-customise_filtered <- customise_filtered[!customise_filtered$player_id %in% all_excluded_ppt, ]
-non_customise_filtered <- non_customise_filtered[!non_customise_filtered$player_id %in% all_excluded_ppt, ]
-
-combined_overall_sutdy_cond_data <- rbind(customise_filtered, non_customise_filtered)
-
-# Levene's Test for Homogeneity of Variances
-perform_levene_test(
-  data = combined_overall_sutdy_cond_data,
-  formula = overall_avg_inflate_after_help ~ study_cond
-)
-
-perform_wilcox_rank_sum_test(
-  data = combined_overall_sutdy_cond_data,
-  formula = overall_avg_inflate_after_help ~ study_cond,
-  "Overall Inflate between study condition"
-)
-
-ggplot(combined_overall_sutdy_cond_data, aes(x = overall_avg_inflate_after_help, y = factor(study_cond), fill = factor(study_cond))) +
-  geom_density_ridges(alpha = 0.4, scale = 0.6) +
-  geom_boxplot(aes(y = factor(study_cond), x = overall_avg_inflate_after_help), width = 0.2, alpha = 0.3) +
-  geom_jitter(height = 0.1, size = 1, alpha = 0.5, color = "black") +
-  labs(
-    title = "Avg inflates after requesting help from robot per study condition",
-    x = "Avg inflates after help requested",
-    y = "Study Condition"
-  ) +
-  theme_minimal() +
-  theme(legend.position = "none", plot.margin = margin(t = 0.5, r = 0.1, b = 0.1, l = 0.5, unit = "cm")) +
-  coord_flip() +
-  scale_y_discrete(expand = c(0, 0), labels = c("non-custom", "custom"))
 
 # Close the database connection
 dbDisconnect(conn)
